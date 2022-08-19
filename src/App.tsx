5d1911c1ed3dbdb60f8e9d8746fc2b19fe56de10
Einav Koren <einavk@wix.com>
2022-08-19 23:09:43 +0300

    enable camera

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
 
 function App() {
+const webCamRef = React.useRef<Webcam>(null);
+const [imgSrc, setImgSrc] = React.useState<string | null>(null);
+
+const capture = React.useCallback(() => {
+  if (webCamRef.current != null)
+    {
+      const img_ = webCamRef.current.getScreenshot();
+      setImgSrc(img_);
+
+    }
+  
+}, [webCamRef, setImgSrc]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Webcam
+        ref={webCamRef}
+        audio={false}
+        height={720}
+        screenshotFormat="image/jpeg"
+        width={1280}
+     />
+     <button onClick={capture}> capture </button>
+     {imgSrc && (
+      <img src={imgSrc} alt={'kaki'}/>
+     )}   
+
       </header>
     </div>
   );
