c40494ea066f3a198921264f61d9c07511b4d9ae
Dean Geckt <t-deangeckt@microsoft.com>
2022-08-20 10:55:20 +0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,27 @@
 import React from 'react';
 import './App.css';
-import Webcam from "react-webcam";
+import Webcam from 'react-webcam';
 
 function App() {
-const webCamRef = React.useRef<Webcam>(null);
-const [imgSrc, setImgSrc] = React.useState<string | null>(null);
+    const webCamRef = React.useRef<Webcam>(null);
+    const [imgSrc, setImgSrc] = React.useState<string | null>(null);
 
-const capture = React.useCallback(() => {
-  if (webCamRef.current != null)
-    {
-      const img_ = webCamRef.current.getScreenshot();
-      setImgSrc(img_);
+    const capture = React.useCallback(() => {
+        if (webCamRef.current != null) {
+            const img_ = webCamRef.current.getScreenshot();
+            setImgSrc(img_);
+        }
+    }, [webCamRef, setImgSrc]);
 
-    }
-  
-}, [webCamRef, setImgSrc]);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-      <Webcam
-        ref={webCamRef}
-        audio={false}
-        height={720}
-        screenshotFormat="image/jpeg"
-        width={1280}
-     />
-     <button onClick={capture}> capture </button>
-     {imgSrc && (
-      <img src={imgSrc} alt={'kaki'}/>
-     )}   
-
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Webcam ref={webCamRef} audio={false} height={720} screenshotFormat="image/jpeg" width={1280} />
+                <button onClick={capture}> capture </button>
+                {imgSrc && <img src={imgSrc} alt={'kaki'} />}
+            </header>
+        </div>
+    );
 }
 
 export default App;
